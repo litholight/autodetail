042e49a311405c00338a6c0355bb2d9da6b17836
Stoneburner Software <sean@stoneburnersoftware.com>
2020-03-07 15:32:35 -0500

    initial draft with few components of site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import styled from 'styled-components'
+
+import logo from './stevelogo.JPG'
+
+const Header = styled.div`
+  color: lightblue;
+  background-color: white;
+  font-size: 2rem;
+  font-weight: bold;
+  padding: 2rem;
+  text-align: center;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <div>Steve & Tannir's</div>
+
+        <img src={logo} width='400px' alt='Logo' />
+      </Header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
