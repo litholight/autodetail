2a5bb292f626df2f439f40593210a5483a1e4da9
Stoneburner Software <sean@stoneburnersoftware.com>
2020-03-09 16:39:41 -0400

    Added basic business info

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ const Header = styled.div`
   background-color: white;
   font-size: 2rem;
   font-weight: bold;
-  padding: 2rem;
+  padding: 1rem;
+  text-align: center;
+`
+
+const Item = styled.li`
+  color: lightblue;
+  font-size: 1rem;
   text-align: center;
 `
 
@@ -18,9 +24,37 @@ function App() {
     <div>
       <Header>
         <div>Steve & Tannir's</div>
-
         <img src={logo} width='400px' alt='Logo' />
+        <div>Stop by, text or call!</div>
+        <div>Tannir 740-343-5865</div>
+        <div>Steve 740-221-5690</div>
       </Header>
+      <div>Available 9-4 Monday thru Thursday</div>
+      <hr />
+      <Header>Services Offered</Header>
+      <Header>Full Service Wash</Header>
+      <ul>
+        <Item>Wash</Item>
+        <Item>Tires</Item>
+        <Item>Shine</Item>
+        <Item>15 minutes</Item>
+      </ul>
+      <Header>Complete Detail</Header>
+      <ul>
+        <Item>Includes full service wash</Item>
+        <Item>Two coats of wax</Item>
+        <Item>Carpet shampoo</Item>
+        <Item>$100 cars, $120 trucks and vans</Item>
+      </ul>
+      <Header>Interior Detail</Header>
+      <ul>
+        <Item>$45</Item>
+      </ul>
+      <Header>Exterior Detail</Header>
+      <ul>
+        <Item>$65</Item>
+      </ul>
+      We use Malco - The Choice of Profressionals
     </div>
   )
 }
