185f277b61f0bd4bf2496aacfeb6710a6520fc4e
Stoneburner Software <sean@stoneburnersoftware.com>
2020-03-12 21:29:42 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/autodetail/precache-manifest.c018d06eede2a0ae0179a3c2a24dedad.js"
+  "/autodetail/precache-manifest.284ceec59079940e445a9ceed80b3c76.js"
 );
 
 self.addEventListener('message', (event) => {
